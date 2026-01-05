c29360ac95c655e1a66a7a107f20de17f4def030
CS144 student <bingo732>
2026-01-05 15:47:26 +0000

    CP2_finish wrapping_integers

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,25 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  // debug("original data {}", n);
+  return Wrap32 { static_cast<uint32_t>( n ) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  // debug("get val {}, checkpoint {}", raw_value_, checkpoint);
+  const uint64_t n_2_32 = 1ULL << 32; // 4294967296
+  const uint32_t abs_seqno = raw_value_ - zero_point.raw_value_;
+  const uint64_t offset = checkpoint & ~( n_2_32 - 1 ); // 高 32 位
+  const uint64_t remain = checkpoint & ( n_2_32 - 1 );  // 低 32 位
+  // debug("have abs_seqno: {}, offset: {}, reamin: {}", abs_seqno, offset, remain);
+  if ( remain > abs_seqno + ( n_2_32 / 2 ) ) {
+    // debug("1-get result {}", abs_seqno + offset + n_2_32);
+    return abs_seqno + offset + n_2_32;
+  } else if ( ( abs_seqno > remain + ( n_2_32 / 2 ) ) && ( abs_seqno + offset >= n_2_32 ) ) {
+    // debug("2-get result {}", abs_seqno + offset - n_2_32);
+    return abs_seqno + offset - n_2_32;
+  }
+  // debug("3-get result {}", abs_seqno + offset);
+  return abs_seqno + offset;
 }
